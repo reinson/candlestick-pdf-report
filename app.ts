e2e1fc0ccaded2f0c1bed39301ad77b796af9155
Tormi Reinson <tormi.reinson@pipedrive.com>
2023-02-19 23:33:27 +0200

    Working ejs

--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import { genrateRoute } from './src/generate-route';
 import { createPDF } from './src/pdf-generator';
+import ejs from 'ejs';
+import path from 'path';
+import { printPdf } from './src/print';
 
 const app = express()
 const port = 3000
@@ -20,4 +23,42 @@ app.get('/generate', genrateRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+
+const passengers = [
+  {
+    name: "Joyce",
+    flightNumber: 7859,
+    time: "18h00",
+  },
+  {
+    name: "Brock",
+    flightNumber: 7859,
+    time: "18h00",
+  },
+  {
+    name: "Eve",
+    flightNumber: 7859,
+    time: "18h00",
+  },
+];
+
+app.get('/ejs', (request, response) => {
+  const filePath = path.join(__dirname, "../src/print.ejs")
+  console.log(filePath);
+  ejs.renderFile(filePath, { passengers }, (err, html) => {
+    if (err) {
+      return response.send('Cannot read file')
+    }
+
+    return response.send(html)
+  })
+
+})
+
+app.get('/print', async (request, response) => {
+  await printPdf();
+
+  response.send('done');
 })
