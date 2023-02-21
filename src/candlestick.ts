536dff5a3fd173c0f9e18d3912abbb17ea1b6ce5
Tormi Reinson <tormi.reinson@pipedrive.com>
2023-02-21 23:29:36 +0200

    Refactored d3 candlestick

--- src/candlestick.ts
+++ src/candlestick.ts
@@ -4,7 +4,7 @@ import { Quote } from './coinapi';
 
 // Based on: https://observablehq.com/@d3/candlestick-chart
 
-export function candlestickChart(data: Quote[], {
+export function candlestickChart(quotesData: Quote[], {
     marginTop = 20, // top margin, in pixels
     marginRight = 30, // right margin, in pixels
     marginBottom = 30, // bottom margin, in pixels
@@ -12,30 +12,19 @@ export function candlestickChart(data: Quote[], {
     width = 800, // outer width, in pixels
     height = 500, // outer height, in pixels
     xRange = [marginLeft, width - marginRight], // [left, right]
-    yType = d3.scaleLinear, // type of y-scale
-    yRange = [height - marginBottom, marginTop], // [bottom, top]
     xFormat = "%b %-d", // a format specifier for the date on the x-axis
     yFormat = "~f", // a format specifier for the value on the y-axis
     yLabel = '', // a label for the y-axis
     stroke = "currentColor", // stroke color for the daily rule
     colors = ["#4daf4a", "#999999", "#e41a1c"] // [up, no change, down]
 } = {}) {
-    // Compute values
-    const X = d3.map(data, (d: Quote) => d.date);
-    const Yo = d3.map(data, (d: Quote) => d.rate.open);
-    const Yc = d3.map(data, (d: Quote) => d.rate.close)
-    const Yh = d3.map(data, (d: Quote) => d.rate.high)
-    const Yl = d3.map(data, (d: Quote) => d.rate.low)
-    const I = d3.range(data.length);
-    const strokeWidth = (xRange[1] - xRange[0]) / X.length * 0.8;
-
-    // Compute default domains and ticks.
-    const xDomain = [d3.min(data, (d: Quote) => d.start), d3.max(data, (d: Quote) => d.end)];
-    const yDomain = [d3.min(Yl), d3.max(Yh)];
+    // Compute domains
+    const xDomain = [d3.min(quotesData, (d: Quote) => d.start), d3.max(quotesData, (d: Quote) => d.end)];
+    const yDomain = [d3.min(quotesData, (d: Quote) => d.rate.low), d3.max(quotesData, (d: Quote) => d.rate.high)];
 
     // Create scales and axis
     const xScale = d3.scaleTime().domain(xDomain).range(xRange);
-    const yScale = yType(yDomain, yRange);
+    const yScale = d3.scaleLinear(yDomain, [height - marginBottom, marginTop]);
     const xAxis = d3.axisBottom(xScale).tickFormat(d3.utcFormat(xFormat));
     const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
 
@@ -73,19 +62,19 @@ export function candlestickChart(data: Quote[], {
     const g = svg.append("g")
         .attr("stroke", stroke)
         .selectAll("g")
-        .data(I)
+        .data(quotesData)
         .join("g")
-        .attr("transform", (i: number) => `translate(${xScale(X[i])},0)`);
+        .attr("transform", (d: Quote) => `translate(${xScale(d.date)},0)`);
 
     g.append("line")
-        .attr("y1", (i: number) => yScale(Yl[i]))
-        .attr("y2", (i: number) => yScale(Yh[i]));
+        .attr("y1", (d: Quote) => yScale(d.rate.low))
+        .attr("y2", (d: Quote) => yScale(d.rate.high));
 
     g.append("line")
-        .attr("y1", (i: number) => yScale(Yo[i]))
-        .attr("y2", (i: number) => yScale(Yc[i]))
-        .attr("stroke-width", strokeWidth)
-        .attr("stroke", (i: number) => colors[1 + Math.sign(Yo[i] - Yc[i])]);
+        .attr("y1", (d: Quote) => yScale(d.rate.open))
+        .attr("y2", (d: Quote) => yScale(d.rate.close))
+        .attr("stroke-width", (xRange[1] - xRange[0]) / quotesData.length * 0.8)
+        .attr("stroke", (d: Quote) => colors[1 + Math.sign(d.rate.open - d.rate.close)]);
 
     const node = body.node() as { innerHTML: string }
 
